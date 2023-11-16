d4e10c9f5e44d587ac07e4a7731064e78cc33663
Meet Mulik <89148021+MeetMulik@users.noreply.github.com>
2023-11-16 17:00:29 +0530

    feat(api): get posts

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -5,7 +5,7 @@ export async function POST(req: Request) {
     const { title, content, links, selectedCategory, imageUrl, publicId } =
         await req.json();
 
-    const authorEmail = 'john@gmail.com'
+    const authorEmail = 'meetmulik125@gmail.com'
 
     if (!title || !content) {
         return NextResponse.json({ message: "Title and content are required" }, { status: 500 })
@@ -31,3 +31,23 @@ export async function POST(req: Request) {
 }
 
 
+export async function GET(req: Request) {
+    try {
+        const posts = await prisma.post.findMany({
+            include: {
+                author: {
+                    select: {
+                        name: true,
+                    }
+                }
+            },
+            orderBy: {
+                createdAt: 'desc'
+            }
+        });
+        return NextResponse.json(posts, { status: 201 });
+
+    } catch (error) {
+        return NextResponse.json({ message: error }, { status: 500 })
+    }
+}
