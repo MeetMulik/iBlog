82c7bae18f3c5525d71304975901f66b9e461813
Meet Mulik <89148021+MeetMulik@users.noreply.github.com>
2023-11-16 16:18:27 +0530

    feat(auth): prisma setup + adapter

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,13 @@
-import { AuthOptions } from "next-auth";
+import prisma from "@/lib/prisma";
 import NextAuth from "next-auth/next";
 import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 
+import { AuthOptions } from "next-auth";
+import { PrismaAdapter } from '@auth/prisma-adapter'
+
 export const authOptions: AuthOptions = {
+    adapter: PrismaAdapter(prisma),
     providers: [
         GitHubProvider({
             clientId: process.env.GITHUB_CLIENT_ID as string,
