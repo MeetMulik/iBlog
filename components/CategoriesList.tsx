3224fa05b6e9ecd768bb49861540a4b729cffa6d
Meet Mulik <89148021+MeetMulik@users.noreply.github.com>
2023-11-16 17:37:17 +0530

    feat: display categories from db

--- components/CategoriesList.tsx
+++ components/CategoriesList.tsx
@@ -1,13 +1,36 @@
 import React from "react";
-import { categoriesData } from "@/data";
 import Link from "next/link";
-const CategoriesList = () => {
+import { TCategory } from '@/types/index';
+
+
+const getCategories = async (): Promise<TCategory[] | null> => {
+    try {
+        const response = await fetch(`${process.env.NEXTAUTH_URL}/api/categories`, {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        if (response.ok) {
+
+            const categories = await response.json();
+            console.log(categories);
+            return categories;
+
+        }
+    } catch (error) {
+        console.log(error);
+    }
+    return null;
+};
+
+const CategoriesList = async () => {
+    const categories = await getCategories();
     return (
         <div className="flex gap-2 text-sm flex-wrap">
-            {categoriesData &&
-                categoriesData.map((category) => (
-                    <Link className="px-4 py-1 rounded-md bg-slate-800 text-white cursor-pointer" key={category.id} href={`/categories/${category.name}`}>
-                        {category.name}
+            {categories &&
+                categories.map((category: TCategory) => (
+                    <Link className="px-4 py-1 rounded-md bg-slate-800 text-white cursor-pointer" key={category.id} href={`/categories/${category.catName}`}>
+                        {category.catName}
                     </Link>
                 ))}
         </div>
