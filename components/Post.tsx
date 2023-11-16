37aa70bd5fc65eec7265eef65040aff7d9bb0972
Meet Mulik <89148021+MeetMulik@users.noreply.github.com>
2023-11-16 20:43:11 +0530

    fix: date display

--- components/Post.tsx
+++ components/Post.tsx
@@ -36,15 +36,23 @@ const Post = async ({
         month: "short",
         day: "numeric",
         year: "numeric",
-    }
+    };
 
     const formattedDate = dateObject.toLocaleDateString("en-US", options);
 
     return (
         <div className="my-4 border-b border-b-300 py-8">
             <div className="mb-4">
-                Posted By: <span className="font-bold">{author} </span>
-                <span>on {formattedDate}</span>
+                {author ? (
+                    <>
+                        Posted By: <span className="font-bold">{author} </span>
+                        <span>on {formattedDate}</span>
+                    </>
+                ) : (
+                    <>
+                        Posted on: <span>{formattedDate}</span>
+                    </>
+                )}
             </div>
             <div className="w-full h-72 relative">
                 {thumbnail ? (
@@ -64,32 +72,48 @@ const Post = async ({
                 )}
             </div>
 
-            {category && <Link className="bg-slate-800 w-fit text-white px-4 py-0.5 text-sm rounded-md mt-4 block" href={`categories/${category}`}>{category}</Link>}
+            {category && (
+                <Link
+                    className="bg-slate-800 w-fit text-white px-4 py-0.5 text-sm rounded-md mt-4 block"
+                    href={`categories/${category}`}
+                >
+                    {category}
+                </Link>
+            )}
             <h2>{title}</h2>
             <p>{content}</p>
-            {
-                links && (
-                    <div className="my-4 flex flex-col gap-3">
-                        {links.map((link, index) => (
-                            <div key={index} className="flex gap-2 items-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244" />
-                                </svg>
+            {links && (
+                <div className="my-4 flex flex-col gap-3">
+                    {links.map((link, index) => (
+                        <div key={index} className="flex gap-2 items-center">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth={1.5}
+                                stroke="currentColor"
+                                className="w-6 h-6"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244"
+                                />
+                            </svg>
 
-                                <Link className="link" href={link}>{link}</Link>
-                            </div>
-                        ))}
-                    </div>
-                )
-            }
-            {
-                isEditable && (
-                    <div className="flex gap-3 font-bold py-2 px-4 rounded-md bg-slate-200 w-fit">
-                        <Link href={`/edit-post/${id}`}>Edit</Link>
-                        <DeleteButton id={id} />
-                    </div>
-                )
-            }
+                            <Link className="link" href={link}>
+                                {link}
+                            </Link>
+                        </div>
+                    ))}
+                </div>
+            )}
+            {isEditable && (
+                <div className="flex gap-3 font-bold py-2 px-4 rounded-md bg-slate-200 w-fit">
+                    <Link href={`/edit-post/${id}`}>Edit</Link>
+                    <DeleteButton id={id} />
+                </div>
+            )}
         </div>
     );
 };
