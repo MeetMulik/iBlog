111094f7f08e4163032e6c6aa67a817b01eb87a8
Meet Mulik <89148021+MeetMulik@users.noreply.github.com>
2023-11-16 11:08:43 +0530

    feat(ui): sign-in buttons

---
+++ app/sign-in/page.tsx
@@ -0,0 +1,11 @@
+import SignInBtns from "@/components/SignInBtns"
+
+const SignIn = () => {
+    return (
+        <div>
+            <SignInBtns />
+        </div>
+    )
+}
+
+export default SignIn
