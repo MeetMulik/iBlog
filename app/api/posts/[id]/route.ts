d4933d867bdf5f011e1166073afa43068506e377
Meet Mulik <89148021+MeetMulik@users.noreply.github.com>
2023-11-16 17:18:28 +0530

    feat(api): update and delete posts

--- app/api/posts/[id]/route.ts
+++ app/api/posts/[id]/route.ts
@@ -15,3 +15,41 @@ export async function GET(req: Request, { params }: { params: { id: string } }) 
     }
     return NextResponse.json({ message: "Hello from posts/[id]/route.ts" });
 }
+
+
+export async function PUT(req: Request, { params }: { params: { id: string } }) {
+    const { title, content, links, selectedCategory, imageUrl, publicId } = await req.json();
+    const id = params.id;
+    try {
+        const updatedPost = await prisma.post.update({
+            where: {
+                id
+            },
+            data: {
+                title,
+                content,
+                links,
+                imageUrl,
+                publicId,
+                catName: selectedCategory,
+            }
+        });
+        return NextResponse.json(updatedPost, { status: 201 });
+    } catch (error) {
+        return NextResponse.json({ message: error }, { status: 500 })
+    }
+}
+
+export async function DELETE(req: Request, { params }: { params: { id: string } }) {
+    const id = params.id;
+    try {
+        const deletedPost = await prisma.post.delete({
+            where: {
+                id
+            }
+        });
+        return NextResponse.json(deletedPost, { status: 201 });
+    } catch (error) {
+        return NextResponse.json({ message: error }, { status: 500 })
+    }
+}
